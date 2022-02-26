c5254b82524437d4adcbcb107a93d9c107370d09
Esdras B Silva <bzr4@icloud.com>
2022-02-26 20:15:17 -0300

    main
    - Remoção de codigo desnecessarios.

--- src/main/java/dev/esdras/diveintodesignpatterns/Main.java
+++ src/main/java/dev/esdras/diveintodesignpatterns/Main.java
@@ -1,18 +1,7 @@
 package dev.esdras.diveintodesignpatterns;
 
-import dev.esdras.diveintodesignpatterns.model.company.GameDevCompany;
-import dev.esdras.diveintodesignpatterns.model.company.OutsourcingCompany;
-import dev.esdras.diveintodesignpatterns.model.transport.CarTransport;
-import dev.esdras.diveintodesignpatterns.model.transport.CombustionEngine;
-import dev.esdras.diveintodesignpatterns.model.transport.Human;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Dive into Design Patterns\n");
-
-        CarTransport carTransport =  new CarTransport(new CombustionEngine(), new Human());
-
-        carTransport.deliver("Araras", "Teste");
-
     }
 }
